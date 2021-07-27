91f9bf90cc1107b8709e39d416bf578c7be14394
Mobeen Abdullah <mobeenabdullah@gmail.com>
2021-07-28 03:38:02 +0500

    Adding setTimeout for getting the loan approved after some time in the App

--- script.js
+++ script.js
@@ -97,6 +97,12 @@ const formatMovementDate = function (date, locale) {
   return new Intl.DateTimeFormat(locale).format(date);
 }
 
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency
+  }).format(value);
+}
 
 const displayMovements = function (acc, sort = false) {
 
@@ -111,11 +117,13 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
 
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
+
     const html = `
         <div class="movements__row">
             <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
             <div class="movements__date">${displayDate}</div>
-            <div class="movements__value">${mov.toFixed(2)}€</div>
+            <div class="movements__value">${formattedMov}</div>
         </div>
     `;
 
@@ -127,26 +135,25 @@ const displayMovements = function (acc, sort = false) {
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 }
 
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements.filter(mov => mov > 0)
       .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements.filter(mov => mov < 0)
       .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements.filter(mov => mov > 0)
       .map(deposit => deposit * acc.interestRate/100)
       .filter(int => int >= 1)
       .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 }
-
 
 
 const createUsernames = function (accs) {
@@ -204,8 +211,6 @@ btnLogin.addEventListener('click', function (e){
       year: 'numeric'
     }
 
-    const locale = navigator.language;
-
     labelDate.textContent = new Intl.DateTimeFormat(currentAccount.locale, options).format(now);
 
     // const day = `${now.getDate()}`.padStart(2, 0);
@@ -259,14 +264,18 @@ btnLoan.addEventListener('click', function (e){
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
-    // Add movement
-    currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Update UI
-    updateUI(currentAccount);
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
+
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
+
   }
 
   inputLoanAmount.value = '';
